5f061d551f97d101e1595f2e5bda944636deea49
justint9696 <justin.tonkinson96@gmail.com>
2023-03-21 21:55:50 -0600

    added button handing and game functions calls

--- src/main.c
+++ src/main.c
@@ -1,14 +1,21 @@
+#include "include/app.h"
+#include "include/game.h"
+#include "include/player.h"
+
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_scancode.h>
 #include <stdio.h>
 
-#include "include/app.h"
-
 int main(int argc, char *argv[]) {
+	int buttons;
 	initSDL();
 
+	Game_Init();
 	printf("%s Initialized.\n", APP_TITLE);
 
 	SDL_Event event;
 	while (1) {
+		buttons = 0;
 		drawRect(0, 0, 15, 15, 0x00000000);
 
 		while (SDL_PollEvent(&event)) {
@@ -17,10 +24,33 @@ int main(int argc, char *argv[]) {
 				printf("%s Terminated.\n", APP_TITLE);
 				exit(0);
 				break;
+			case SDL_KEYDOWN:
+				switch (event.key.keysym.scancode) {
+				case SDL_SCANCODE_UP:
+					buttons |= BUTTON_UP;
+					break;
+				case SDL_SCANCODE_DOWN:
+					buttons |= BUTTON_DOWN;
+					break;
+				case SDL_SCANCODE_LEFT:
+					buttons |= BUTTON_LEFT;
+					break;
+				case SDL_SCANCODE_RIGHT:
+					buttons |= BUTTON_RIGHT;
+					break;
+				case SDL_SCANCODE_SPACE:
+					buttons |= BUTTON_SPACE;
+					break;
+				default:
+					break;
+				}
+				break;
 			default:
 				break;
 			}
 		}
+
+		Game_Main(buttons);
 	}
 
 	return 0;
