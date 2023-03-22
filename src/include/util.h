d8e0198ba5e876c7f32dfc49dbb2dbeb0cd9fe72
justint9696 <justin.tonkinson96@gmail.com>
2023-03-21 21:54:40 -0600

    Intial commit

---
+++ src/include/util.h
@@ -0,0 +1,6 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+int clamp(int min, int current, int max);
+
+#endif
---
+++ src/util.c
@@ -0,0 +1,5 @@
+#include "include/util.h"
+
+int clamp(int min, int current, int max) {
+    return (current < min ? min : current > max ? max : current);
+}
