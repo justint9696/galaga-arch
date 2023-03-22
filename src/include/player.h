f7aa2987561958826c79bca02125abbb04dedf9f
justint9696 <justin.tonkinson96@gmail.com>
2023-03-21 21:55:35 -0600

    added init and update functions

--- src/include/player.h
+++ src/include/player.h
@@ -3,4 +3,26 @@
 
 #include "entity.h"
 
+#define BUTTON_UP           (1 << 0)
+#define BUTTON_DOWN         (1 << 1)
+#define BUTTON_LEFT         (1 << 2)
+#define BUTTON_RIGHT        (1 << 3)
+#define BUTTON_SPACE        (1 << 4)
+
+#define PLAYER_SPAWN_X      0.f
+#define PLAYER_SPAWN_Y      0.f
+#define PLAYER_VEL          15
+#define PLAYER_WIDTH        15.f
+#define PLAYER_HEIGHT       15.f
+#define PLAYER_TEXTURE      "images/player.png"
+
+typedef struct {
+    int buttons;
+    Entity *ent;
+} Player;
+
+void Player_Init();
+
+void Player_Update(int buttons);
+
 #endif
--- src/player.c
+++ src/player.c
@@ -1,1 +1,18 @@
+#include "include/entity.h"
 #include "include/player.h"
+
+static Player player;
+
+void Player_Init() {
+    player.buttons = 0;
+
+    Entity *self = Entity_Init(PLAYER_SPAWN_X, PLAYER_SPAWN_Y, PLAYER_WIDTH, PLAYER_HEIGHT, PLAYER_TEXTURE);
+    player.ent = self;
+}
+
+void Player_Update(int buttons) {
+    player.buttons = buttons;
+
+    if (buttons & BUTTON_UP)
+        Entity_SetVelocity(player.ent, 0, PLAYER_VEL);
+}
