eb4cdf956bf98705b47d6af72923083c19b2ad49
justint9696 <justin.tonkinson96@gmail.com>
2023-03-23 10:20:23 -0600

    button handling for key release

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 #include <SDL2/SDL_scancode.h>
 
 int main(int argc, char *argv[]) {
-	int buttons;
+	int buttons = 0;
 
 	initSDL();
 
@@ -16,14 +16,13 @@ int main(int argc, char *argv[]) {
 
 	SDL_Event event;
 	while (1) {
-		buttons = 0;
-
 		while (SDL_PollEvent(&event)) {
 			switch (event.type) {
 			case SDL_QUIT:
 				printf("%s Terminated.\n", APP_TITLE);
 				exit(0);
 				break;
+
 			case SDL_KEYDOWN:
 				switch (event.key.keysym.scancode) {
 				case SDL_SCANCODE_UP:
@@ -45,6 +44,29 @@ int main(int argc, char *argv[]) {
 					break;
 				}
 				break;
+
+			case SDL_KEYUP:
+				switch (event.key.keysym.scancode) {
+				case SDL_SCANCODE_UP:
+					buttons &= ~BUTTON_UP;
+					break;
+				case SDL_SCANCODE_DOWN:
+					buttons &= ~BUTTON_DOWN;
+					break;
+				case SDL_SCANCODE_LEFT:
+					buttons &= ~BUTTON_LEFT;
+					break;
+				case SDL_SCANCODE_RIGHT:
+					buttons &= ~BUTTON_RIGHT;
+					break;
+				case SDL_SCANCODE_SPACE:
+					buttons &= ~BUTTON_SPACE;
+					break;
+				default:
+					break;
+				}
+				break;
+
 			default:
 				break;
 			}
