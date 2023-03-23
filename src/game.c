2651521f79928de03d35abcfc158ef72aaea70d5
justint9696 <justin.tonkinson96@gmail.com>
2023-03-22 19:32:59 -0600

    main and init implementations

--- src/game.c
+++ src/game.c
@@ -1,10 +1,17 @@
 #include "include/game.h"
+#include "include/entity.h"
 #include "include/player.h"
+#include "include/render.h"
+
+#include <SDL2/SDL_render.h>
 
 void Game_Init() {
-
+    Player_Init();
 }
 
 void Game_Main(int buttons) {
+    Renderer_Prepare();
     Player_Update(buttons);
+    Entity_UpdateAll();
+    Renderer_Update();
 }
