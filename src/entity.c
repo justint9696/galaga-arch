74aeb9dd6546aebd2f3ffc0f3b847828b40d3a78
justint9696 <justin.tonkinson96@gmail.com>
2023-03-25 11:12:21 -0600

    entity fire

--- src/entity.c
+++ src/entity.c
@@ -14,6 +14,7 @@ static LinkedList _entities;
 static inline Entity *_Entity_Alloc() {
     Entity *self = (Entity *)malloc(sizeof(Entity));
     memset(self, 0, sizeof(Entity));
+    self->id = _num_ent++;
 
     LinkedList_Add(&_entities, (void *)self);
 
@@ -25,25 +26,34 @@ static inline void _Entity_Free(Entity *self) {
 
     LinkedList_Remove(&_entities, &self);
     free(self);
+
+    --_num_ent;
 }
 
-static void _Entity_Render(Entity *self) {
+static void _Entity_Render_Rect(Entity *self) {
+    DrawRect(self->pos.x, self->pos.y, self->width, self->height, self->color);
+}
+
+static void _Entity_Render_Texure(Entity *self) {
     DrawTexture(self->texture, self->pos.x, self->pos.y, self->width, self->height);
 }
 
 static inline void _Entity_Update(Entity *self, uint64_t deltaTime) {
     // update entity position
+    printf("test %i\n", self->id);
     self->pos.x += round(1.f * deltaTime * self->vel.x),
     self->pos.x = clamp(0, self->pos.x, WINDOW_WIDTH - self->width);
 
+    printf("test %i\n", self->id);
     self->pos.y += round(1.f * deltaTime * self->vel.y);
     self->pos.y = clamp(0, self->pos.y, WINDOW_HEIGHT - self->height);
 
     #ifdef DEBUG
-        printf("\nentity debug information: \n\t- id: %i \n\t- origin (%.2f, %.2f) \n\t- velocity: (%.2f, %.2f)\n", self->id, self->pos.x, self->pos.y, self->vel.x, self->vel.y);
+        //printf("\nentity debug information: \n\t- id: %i \n\t- origin (%.2f, %.2f) \n\t- velocity: (%.2f, %.2f)\n", self->id, self->pos.x, self->pos.y, self->vel.x, self->vel.y);
     #endif
-
-    _Entity_Render(self);
+    
+    printf("test %i\n", self->id);
+    ((void(*)(Entity *))self->render)(self);
 }
 
 void Entity_UpdateAll(uint64_t deltaTime) {
@@ -51,17 +61,22 @@ void Entity_UpdateAll(uint64_t deltaTime) {
     LinkedList *tmp = &_entities;
 
     while (tmp) {
+        //printf("test %i hello\n", _num_ent);
         entity = (Entity *)tmp->item;
+        printf("test %i assign\n", entity->id);
         _Entity_Update(entity, deltaTime);
 
+        //printf("test %i %i\n", _num_ent, entity->id);
         tmp = tmp->next;
+        //printf("test %i goodbye\n", _num_ent);
     }
 }
 
-Entity *Entity_Init(float x, float y, int width, int height, const char *texture) {
+Entity *Entity_Init(type_t type, team_t team, float x, float y, int width, int height, const char *texture) {
     Entity *self = _Entity_Alloc();
 
-    self->id = _num_ent++;
+    self->type = type;
+    self->team = team;
 
     self->pos.x = x;
     self->pos.y = y;
@@ -69,7 +84,9 @@ Entity *Entity_Init(float x, float y, int width, int height, const char *texture
     self->width = width;
     self->height = height;
 
-    self->texture = LoadTexture(texture);
+    self->texture = texture == NULL ? NULL : LoadTexture(texture);
+
+    self->render = texture == NULL ? &_Entity_Render_Rect : &_Entity_Render_Texure;
 
     #ifdef DEBUG
         printf("entity %i initialized.\n", self->id);
@@ -78,20 +95,24 @@ Entity *Entity_Init(float x, float y, int width, int height, const char *texture
     return self;
 }
 
-void Entity_SetPosition(Entity *self, float x, float y) {
-    self->pos.x = x;
-    self->pos.y = y;
+void Entity_SetPosition(Entity *self, vec2 pos) {
+    memcpy(&self->pos, &pos, sizeof(vec2));
 
     #ifdef DEBUG
         printf("entity %i's position updated.\n", self->id);
     #endif
 }
 
-void Entity_SetVelocity(Entity *self, float vx, float vy) {
-    self->vel.x = vx;
-    self->vel.y = vy;
+void Entity_SetVelocity(Entity *self, vec2 vel) {
+    memcpy(&self->vel, &vel, sizeof(vec2));
 
     #ifdef DEBUG
         printf("entity %i's velocity updated.\n", self->id);
     #endif
+}
+
+void Entity_Fire(team_t team, vec2 pos, vec2 vel) {
+    Entity *self = Entity_Init(TYPE_PROJECTILE, team, pos.x, pos.y, BULLET_WIDTH, BULLET_HEIGHT, BULLET_TEXTURE);
+
+    Entity_SetVelocity(self, vel);
 }
--- src/include/entity.h
+++ src/include/entity.h
@@ -4,24 +4,45 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 
+#define BULLET_TEXTURE      NULL
+#define BULLET_VELOCITY     1.f
+#define BULLET_WIDTH        5.f
+#define BULLET_HEIGHT       15.f
+
+typedef enum {
+    TYPE_PLAYER,
+    TYPE_ENEMY,
+    TYPE_PROJECTILE,
+} type_t;
+
+typedef enum {
+    TEAM_ALLY,
+    TEAM_ENEMY,
+} team_t;
+
 typedef struct {
     float x, y;
 } vec2;
 
 typedef struct {
     int id;
+    type_t type;
+    team_t team;
     int width, height;
     vec2 pos, vel;
     SDL_Texture *texture;
     uint32_t color;
+    void *render;
 } Entity;
 
-Entity *Entity_Init(float x, float y, int width, int height, const char *texture);
+Entity *Entity_Init(type_t type, team_t team, float x, float y, int width, int height, const char *texture);
 
 void Entity_UpdateAll(uint64_t deltaTime);
 
-void Entity_SetPosition(Entity *self, float x, float y);
+void Entity_SetPosition(Entity *self, vec2 pos);
 
-void Entity_SetVelocity(Entity *self, float vx, float vy);
+void Entity_SetVelocity(Entity *self, vec2 vel);
+
+void Entity_Fire(team_t team, vec2 pos, vec2 vel);
 
 #endif
