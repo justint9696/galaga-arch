635ad585b1ac47590f0068040db057980abe7a89
justint9696 <justin.tonkinson96@gmail.com>
2023-03-29 00:36:00 -0600

    init and update functions for stars

--- src/game.c
+++ src/game.c
@@ -3,41 +3,37 @@
 #include "inc/enemy.h"
 #include "inc/player.h"
 #include "inc/render.h"
+#include "inc/stars.h"
+#include "inc/time.h"
 
 #include <SDL2/SDL_render.h>
-#include <sys/time.h>
 
 static uint64_t _time, _delta_time;
 
-static inline uint64_t _Get_Tick() {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    const uint64_t tick = tv.tv_sec * 1000LL + tv.tv_usec / 1000;
-    return tick;
-}
-
 static inline void _Game_Tick() {
-    const uint64_t tick = _Get_Tick();
+    const uint64_t tick = Get_Tick();
 
     _delta_time = tick - _time;
     _time += _delta_time;
 }
 
-inline uint64_t Time_Passed(uint64_t tick) {
-    const uint64_t time = _Get_Tick() - tick;
-    return time;
-}
-
 void Game_Init() {
-    _time = _Get_Tick();
+    // store current tick
+    _time = Get_Tick();
+
+    // prepare entities
     Entity_InitList();
     Player_Init();
     Enemy_InitAll(1);
+
+    // prepare gfx
+    Stars_Init();
 }
 
 void Game_Main(int buttons) {
     _Game_Tick();
     Renderer_Prepare();
+    Stars_Update(_time, _delta_time);
     Player_Update(buttons, _time);
     Entity_UpdateAll(_delta_time);
     Renderer_Update();
