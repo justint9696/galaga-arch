55ad4f174b13f22bc43364e34fa965d5a41227a1
justint9696 <justin.tonkinson96@gmail.com>
2023-04-02 20:12:10 -0600

    hex to color struct function

--- src/inc/util.h
+++ src/inc/util.h
@@ -1,10 +1,15 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 
+#include "type.h"
+
 #include <stdbool.h>
+#include <stdint.h>
 
 #define DEBUG
 
 float clamp(int min, float current, int max);
+
+void RGBA(uint32_t src, Color *dst);
 
 #endif
--- src/util.c
+++ src/util.c
@@ -3,3 +3,10 @@
 float clamp(int min, float current, int max) {
     return (current < min ? min : current > max ? max : current);
 }
+
+void RGBA(uint32_t src, Color *dst) {
+    dst->r = ((src >> 24) & 0xFF);
+    dst->g = ((src >> 16) & 0xFF);
+    dst->b = ((src >> 8) & 0xFF);
+    dst->a = ((src) & 0xFF);
+}
