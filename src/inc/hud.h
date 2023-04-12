537bfdd2289858ce3c1a6c4882a9dea7a512cfa6
justint9696 <justin.tonkinson96@gmail.com>
2023-04-11 18:27:25 -0600

    change assets path

--- src/inc/hud.h
+++ src/inc/hud.h
@@ -1,8 +1,7 @@
 #ifndef _HUD_H_
 #define _HUD_H_
 
-#define HUD_FONT        ""
-#define FONT_PATH        "fonts/press-start-2p-font/font.ttf"
+#define FONT_PATH        "assets/fonts/press-start-2p-font/font.ttf"
 
 void Hud_Init();
 
