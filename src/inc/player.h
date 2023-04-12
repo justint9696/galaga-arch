edb48f7dcc1f81ae749a962956cf5295ce7dc185
justint9696 <justin.tonkinson96@gmail.com>
2023-04-11 18:27:08 -0600

    player util functions & renaming

--- src/inc/player.h
+++ src/inc/player.h
@@ -4,17 +4,20 @@
 #include "entity.h"
 #include "window.h"
 
+#include <stdbool.h>
+
 #define BUTTON_UP           (1 << 0)
 #define BUTTON_DOWN         (1 << 1)
 #define BUTTON_LEFT         (1 << 2)
 #define BUTTON_RIGHT        (1 << 3)
 #define BUTTON_SPACE        (1 << 4)
 
-#define PLAYER_TEXTURE	    "images/player.png"	
+#define PLAYER_TEXTURE	    "assets/images/player.png"	
 #define PLAYER_WIDTH        66.7f
 #define PLAYER_HEIGHT       48.f
 
 #define PLAYER_VELOCITY     0.35f
+#define PLAYER_SPAWN_HEALTH 1.f
 #define PLAYER_SPAWN_X      ((WINDOW_WIDTH - PLAYER_WIDTH) / 2)
 #define PLAYER_SPAWN_Y      50.f
 
@@ -23,11 +26,13 @@ typedef struct {
     Entity *ent;
 } Player;
 
-vec2 Player_GetPosition();
+bool Player_IsAlive();
 
-vec2 Player_GetVelocity();
+vec2 Player_Position();
 
-void Player_Init();
+vec2 Player_Velocity();
+
+void Player_Init(uint64_t tick);
 
 void Player_Update(int buttons, uint64_t tick);
 
--- src/player.c
+++ src/player.c
@@ -4,22 +4,30 @@
 static Player _self;
 static vec2 _vel;
 
-vec2 Player_GetPosition() {
+bool Player_IsAlive() {
+    return (_self.ent->state != STATE_DEAD);
+}
+
+vec2 Player_Position() {
     return _self.ent->pos;
 }
 
-vec2 Player_GetVelocity() {
+vec2 Player_Velocity() {
     return _self.ent->vel;
 }
 
-void Player_Init() {
+void Player_Init(uint64_t tick) {
     memset(&_self, 0, sizeof(Player));
-    _self.ent = Entity_Init(TYPE_PLAYER, TEAM_ALLY, PLAYER_SPAWN_X, PLAYER_SPAWN_Y, PLAYER_WIDTH, PLAYER_HEIGHT, PLAYER_TEXTURE);
+    _self.ent = Entity_Init(TYPE_PLAYER, TEAM_ALLY, PLAYER_SPAWN_HEALTH, PLAYER_SPAWN_X, PLAYER_SPAWN_Y, PLAYER_WIDTH, PLAYER_HEIGHT, PLAYER_TEXTURE);
+    _self.ent->tick = tick;
 
     printf("Player initialized.\n");
 }
 
 void Player_Update(int buttons, uint64_t tick) {
+    if (_self.ent->state == STATE_DEAD)
+        return;
+
     if (buttons & BUTTON_SPACE) 
         Entity_Fire(_self.ent, tick);
     
