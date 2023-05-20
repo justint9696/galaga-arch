1252a66a7c574bc0bc351eade0cc273b526b62c9
justint9696 <justin.tonkinson96@gmail.com>
2023-05-19 22:44:49 -0600

    add button monitoring from main

--- src/buttons.c
+++ src/buttons.c
@@ -1,9 +1,55 @@
 #include "inc/buttons.h"
+#include "inc/player.h"
+
+int _buttons = 0;
 
 int Buttons_Get() {
-        return 0;
+    return _buttons;
 }
 
-void Buttons_Update() {
+void Buttons_Update(SDL_Event event) {
+    switch (event.type) {
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.scancode) {
+                case SDL_SCANCODE_UP:
+                    _buttons |= BUTTON_UP;
+                    break;
+                case SDL_SCANCODE_DOWN:
+                    _buttons |= BUTTON_DOWN;
+                    break;
+                case SDL_SCANCODE_LEFT:
+                    _buttons |= BUTTON_LEFT;
+                    break;
+                case SDL_SCANCODE_RIGHT:
+                    _buttons |= BUTTON_RIGHT;
+                    break;
+                case SDL_SCANCODE_SPACE:
+                    _buttons |= BUTTON_SPACE;
+                    break;
+                default:
+                    break;
+            }
+            break;
 
+        case SDL_KEYUP:
+            switch (event.key.keysym.scancode) {
+                case SDL_SCANCODE_UP:
+                    _buttons &= ~BUTTON_UP;
+                    break;
+                case SDL_SCANCODE_DOWN:
+                    _buttons &= ~BUTTON_DOWN;
+                    break;
+                case SDL_SCANCODE_LEFT:
+                    _buttons &= ~BUTTON_LEFT;
+                    break;
+                case SDL_SCANCODE_RIGHT:
+                    _buttons &= ~BUTTON_RIGHT;
+                    break;
+                case SDL_SCANCODE_SPACE:
+                    _buttons &= ~BUTTON_SPACE;
+                    break;
+                default:
+                    break;
+            }
+    }
 }
--- src/inc/buttons.h
+++ src/inc/buttons.h
@@ -1,8 +1,10 @@
 #ifndef _BUTTONS_H_
 #define _BUTTONS_H_
 
+#include <SDL2/SDL_events.h>
+
 int Buttons_Get();
 
-void Buttons_Update();
+void Buttons_Update(SDL_Event event);
 
 #endif
