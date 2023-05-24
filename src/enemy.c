00128986b381cbdff46d5fc268b31eb779f7d305
justint9696 <justin.tonkinson96@gmail.com>
2023-05-24 01:12:40 -0600

    added enemy struct

--- src/enemy.c
+++ src/enemy.c
@@ -5,78 +5,96 @@
 #include "inc/player.h"
 #include "inc/util.h"
 
+#include <assert.h>
+
 static int _count;
-static Entity **_entities;
+static Enemy **_enemies;
 
-static inline Entity *_Enemy_Init() {
-    return Entity_Init(TYPE_ENEMY, TEAM_ENEMY, ENEMY_SPAWN_HEALTH, -ENEMY_WIDTH, ENEMY_SPAWN_Y - 250.f, ENEMY_WIDTH, ENEMY_HEIGHT, ENEMY_TEXTURE);
-}
+static inline Enemy *_Enemy_Init() {
+    Enemy *self = (Enemy *)malloc(sizeof(Enemy));
+    memset(self, 0, sizeof(Enemy));
 
-static void _Enemy_ResetPath(Entity *self) {
+    self->ent = Entity_Init(TYPE_ENEMY, TEAM_ENEMY, ENEMY_SPAWN_HEALTH, -ENEMY_WIDTH, ENEMY_SPAWN_Y - 250.f, ENEMY_WIDTH, ENEMY_HEIGHT, ENEMY_TEXTURE);
     self->state = STATE_IDLE;
-    memset(&self->path, 0, sizeof(path_s));
+
+    return self;
 }
 
-static void _Enemy_ThinkPath(Entity *self) {
+static void _Enemy_ResetPath(Enemy *self) {
+    self->ent->state = STATE_IDLE;
+    queue_free(&self->path);
+}
+
+static void _Enemy_ThinkPath(Enemy *self) {
+    path_s *path = (path_s *)malloc(sizeof(path_s));
+
     switch (self->state) {
     case STATE_IDLE:
-        self->path.dst = self->pos;
+        path->dst = self->ent->pos;
 
-        if (self->pos.x < WINDOW_WIDTH - self->width)
-            self->path.dst.x = WINDOW_WIDTH - self->width;
+        if (self->ent->pos.x < WINDOW_WIDTH - self->ent->width)
+            path->dst.x = WINDOW_WIDTH - self->ent->width;
         else
-            self->path.dst.x = 0.f;
+            path->dst.x = 0.f;
 
-        Path_Linear(self, self->pos, self->path.dst, ENEMY_VELOCITY);
+        Path_Linear(self->ent, self->ent->pos, path->dst, ENEMY_VELOCITY);
 
-        self->path.type = PATH_LINEAR;
+        path->type = PATH_LINEAR;
         break;
     case STATE_SPAWN:
-        self->path.org = self->pos;
+        path->org = self->ent->pos;
 
-        self->path.dst.x = ENEMY_SPAWN_X;
-        self->path.dst.y = ENEMY_SPAWN_Y;
+        path->dst.x = ENEMY_SPAWN_X;
+        path->dst.y = ENEMY_SPAWN_Y;
             
-        Path_Bezier(self, self->path.org, self->path.dst, ENEMY_VELOCITY);
+        Path_Linear(self->ent, path->org, path->dst, ENEMY_VELOCITY);
 
-        self->path.type = PATH_BEZIER;
+        path->type = PATH_LINEAR;
         break;
     }
 
-    self->state = STATE_TRAVEL;
+    printf("path queued\n");
+    enqueue(&self->path, (void *)path);
+    self->ent->state = STATE_TRAVEL;
 }
 
-static void _Enemy_TravelPath(Entity *self) {
-    // TODO: decide enemy pathing here    
-    Hud_AddText("Entity Path: %s", self->path.type == PATH_LINEAR ? "Linear" : self->path.type == PATH_BEZIER ? "Bezier" : "Circular");
-    switch (self->path.type) {
+static void _Enemy_TravelPath(Enemy *self) {
+    path_s *path = (path_s *)queue_front(&self->path);
+    assert(path);
+
+    switch (path->type) {
     case PATH_LINEAR:
+        Path_Linear(self->ent, path->org, path->dst, ENEMY_VELOCITY);
         break;
     case PATH_CIRCULAR:
-        Path_Circular(self, self->path.org, self->path.dst, ENEMY_VELOCITY);
+        Path_Circular(self->ent, path->org, path->dst, ENEMY_VELOCITY);
         break;
     case PATH_BEZIER:
-        Path_Bezier(self, self->path.org, self->path.dst, ENEMY_VELOCITY);
+        Path_Bezier(self->ent, path->org, path->dst, ENEMY_VELOCITY);
         break;
     }
 
-    if (Distance(self->pos, self->path.dst) < .5f)
-        _Enemy_ResetPath(self);
+    if (Distance(self->ent->pos, path->dst) < .5f) {
+        dequeue(&self->path);
+        self->state = STATE_IDLE;
+    }
 }
 
-static void _Enemy_ThinkAttack(Entity *self, uint64_t tick) {
+static void _Enemy_ThinkAttack(Enemy *self, uint64_t tick) {
     const vec2 
         pos = Player_Position(),
         vel = Player_Velocity();
 
+    Entity *ent = self->ent;
+
     const float
-        dx = (pos.x - self->pos.x),
-        dy = fabs(pos.y - self->pos.y);
+        dx = (pos.x - ent->pos.x),
+        dy = fabs(pos.y - ent->pos.y);
 
     // player is not moving
     if (vel.x == 0.f) {
         if (fabs(dx) < 5.f)
-            Entity_Fire(self, tick);
+            Entity_Fire(ent, tick);
 
         return;
     }
@@ -90,10 +108,10 @@ static void _Enemy_ThinkAttack(Entity *self, uint64_t tick) {
         ty = fabs(dy / BULLET_VELOCITY);
 
    if (fabs(dx) < 30.f || (fabs(tx - ty) < 1.f))
-        Entity_Fire(self, tick);
+        Entity_Fire(ent, tick);
 }
 
-static void _Enemy_Think(Entity *self, uint64_t tick) {
+static void _Enemy_Think(Enemy *self, uint64_t tick) {
     state_t p_state = self->state;
     _Enemy_ThinkAttack(self, tick);
     
@@ -101,6 +119,7 @@ static void _Enemy_Think(Entity *self, uint64_t tick) {
     case STATE_IDLE:
     case STATE_SPAWN:
         _Enemy_ThinkPath(self);
+        self->state = STATE_TRAVEL;
         break;
     case STATE_ATTACK:
         // return to state prior attack
@@ -115,29 +134,29 @@ static void _Enemy_Think(Entity *self, uint64_t tick) {
 }
 
 void Enemy_InitAll(uint64_t tick) {
-    Entity *entity;
+    Enemy *enemy;
     _count = Level_EnemyCount();
-    _entities = (Entity **)malloc(sizeof(Entity *) * _count);
+    _enemies = (Entity **)malloc(sizeof(Entity *) * _count);
     
     for (int i = 0; i < _count; i++) {
-        entity = _Enemy_Init(tick);
-        entity->state = ENEMY_START_STATE;
-        entity->tick = tick;
-        _entities[i] = entity;
+        enemy = _Enemy_Init(tick);
+        enemy->state = ENEMY_START_STATE;
+        enemy->ent->tick = tick;
+        _enemies[i] = enemy;
     }
 
     printf("Enemies initialized.\n");
 }
 
 void Enemy_UpdateAll(uint64_t tick) {
-    Entity *entity;
+    Enemy *enemy;
     
     for (int i = 0; i < _count; i++) {
-        entity = _entities[i];
+        enemy = _enemies[i];
 
-        if (!entity || !Entity_IsAlive(entity))
+        if (!enemy || !Entity_IsAlive(enemy->ent))
             continue;
 
-        _Enemy_Think(entity, tick);
+        _Enemy_Think(enemy, tick);
     }
 }
--- src/inc/enemy.h
+++ src/inc/enemy.h
@@ -2,6 +2,8 @@
 #define _ENEMY_H_
 
 #include "entity.h"
+#include "path.h"
+#include "queue.h"
 #include "window.h"
 
 #define ENEMY_TEXTURE       "assets/images/enemy01.png"
@@ -12,6 +14,19 @@
 #define ENEMY_SPAWN_X       ((WINDOW_WIDTH - ENEMY_WIDTH) / 2)
 #define ENEMY_SPAWN_Y       (WINDOW_HEIGHT - ENEMY_HEIGHT - 50.f)
 #define ENEMY_VELOCITY      0.25f
+
+typedef enum {
+    STATE_IDLE,
+    STATE_SPAWN,
+    STATE_TRAVEL,
+    STATE_ATTACK,
+} estate_t;
+
+typedef struct {
+    estate_t state;
+    Queue path;
+    Entity *ent;
+} Enemy;
 
 void Enemy_InitAll(uint64_t tick);
 
