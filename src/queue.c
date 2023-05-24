5d30a593793a85b2f4a2d1d07457f2e8e3b5e0a8
justint9696 <justin.tonkinson96@gmail.com>
2023-05-24 01:12:08 -0600

    Initial commit

---
+++ src/queue.c
@@ -0,0 +1,38 @@
+#include "inc/queue.h"
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+Queue *queue_init() {
+    Queue *q = (Queue *)malloc(sizeof(Queue));
+    memset(q, 0, sizeof(Queue));
+    return q;
+}
+
+void queue_free(Queue *q) {
+    for (int i = q->index; i < q->size; i++) 
+        free(q->queue[i]);
+
+    memset(q, 0, sizeof(Queue));
+}
+
+void *queue_front(const Queue *q) {
+    return q->queue[q->index];
+}
+
+void *queue_rear(const Queue *q) {
+    return q->queue[(q->index + q->size - 1) % MAX_QUEUE];
+}
+
+void enqueue(Queue *q, void *item) {
+    assert(q->size < MAX_QUEUE);
+    q->queue[q->index + q->size++] = item;
+}
+
+void dequeue(Queue *q) {
+    assert(q->size > 0);
+    q->index = (++q->index % MAX_QUEUE);
+    --q->size;
+}
+
