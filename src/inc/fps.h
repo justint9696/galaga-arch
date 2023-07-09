91a6607ebcb52752844d99983b02fa22dc852fc4
justint9696 <justin.tonkinson96@gmail.com>
2023-07-09 14:56:13 -0500

    modified fps struct and functions

--- src/inc/fps.h
+++ src/inc/fps.h
@@ -1,11 +1,21 @@
 #ifndef _FPS_H_
 #define _FPS_H_
 
+#include <stdint.h>
+
 #define FPS_TARGET      60
 #define FRAME_DELAY     (1000.f / FPS_TARGET)
 
 typedef struct {
-    int previous, frames;
+    int frames;
+    uint64_t start, end;
 } fps_s;
+
+void frame_start();
+void frame_end();
+
+float fps_get();
+void fps_init();
+void fps_limit();
 
 #endif
