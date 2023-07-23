54a248cc98043b5c957db6620f722c546de211b9
justint9696 <justin.tonkinson96@gmail.com>
2023-07-23 00:19:17 -0600

    modified pathing routes

--- src/route.c
+++ src/route.c
@@ -26,35 +26,38 @@ static inline void _Route_Append(Queue *q, vec2 dst, float speed, ptype_t type) 
 
 inline void Route_Spawn(Queue *q, vec2 org) {
     float radius = 50.f;
-    _Route_Start(q, org, (vec2) { 50, ENEMY_SPAWN_Y - 100.f }, ENEMY_SPAWN_VELOCITY, PATH_LINEAR);
-    _Route_Append(q, (vec2) { _rear(q).x, _rear(q).y - radius }, -ENEMY_SPAWN_VELOCITY, PATH_CIRCULAR);
-    _Route_Append(q, (vec2) { ENEMY_SPAWN_X, ENEMY_SPAWN_Y }, ENEMY_SPAWN_VELOCITY, PATH_BEZIER);
+    float speed = ENEMY_SPAWN_VELOCITY;
+    _Route_Start(q, org, (vec2) { 50, ENEMY_SPAWN_Y - 100.f }, speed, PATH_CIRCULAR);
+    _Route_Append(q, (vec2) { _rear(q).x, _rear(q).y - radius }, speed, PATH_CIRCULAR);
+    _Route_Append(q, (vec2) { ENEMY_SPAWN_X, ENEMY_SPAWN_Y }, speed, PATH_BEZIER);
 }
 
 inline void Route_Idle(Queue *q, vec2 org) {
     path_s *path = Path_Init();
 
-    path->org = org;
+    memcpy(&path->org, &org, sizeof(vec2));
+    memcpy(&path->dst, &path->org, sizeof(vec2));
+
     if (org.x < WINDOW_WIDTH - ENEMY_WIDTH) 
         path->dst.x = WINDOW_WIDTH - ENEMY_WIDTH;
     else
         path->dst.x = 0.f;
 
-    path->dst.y = path->org.y;
     path->type = PATH_LINEAR;
     path->speed = ENEMY_IDLE_VELOCITY;
+    path->orientation = ORIENTATE_UPRIGHT;
     enqueue(q, path);
 }
 
 inline void Route_Swoop(Queue *q, vec2 org) {
-    float speed = 0.25f;
+    float speed = ENEMY_SPAWN_VELOCITY;
     float radius = 100.f;
-    _Route_Start(q, org, (vec2) { org.x, org.y + (radius) }, speed, PATH_CIRCULAR);
-    _Route_Append(q, org, speed, PATH_CIRCULAR);
+    _Route_Start(q, org, (vec2) { org.x, org.y + (radius) }, -speed, PATH_CIRCULAR);
+    _Route_Append(q, org, -speed, PATH_CIRCULAR);
     _Route_Append(q, (vec2) { org.x - (2.f * radius), org.y - (1.5 * radius) }, speed, PATH_BEZIER);
-    _Route_Append(q, (vec2) { org.x + (2.f * radius), org.y - (3.0 * radius) }, speed, PATH_BEZIER);
+    _Route_Append(q, (vec2) { org.x + (2.f * radius), org.y - (3.0 * radius) }, -speed, PATH_BEZIER);
 
     // keep here
-    _Route_Append(q, org, speed, PATH_BEZIER);
+    _Route_Append(q, org, speed, PATH_CIRCULAR);
 }
 
