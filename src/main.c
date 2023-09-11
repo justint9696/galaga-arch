fce1ebf134bfad334b5e2483dc7d163213dc9f00
justint9696 <justin.tonkinson96@gmail.com>
2023-09-10 19:37:01 -0600

    game_destroy on quit & header cleanup

--- src/main.c
+++ src/main.c
@@ -3,26 +3,21 @@
 #include "game/buttons.h"
 #include "game/game.h"
 
-#include "entity/player.h"
-
 #include <SDL2/SDL_events.h>
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_scancode.h>
 
-void Poll_Events(Player *player) {
+void Poll_Events(Game *game) {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
         switch (event.type) {
             case SDL_QUIT:
                 printf("%s Terminated.\n", GAME_TITLE);
+                Game_Destroy(game);
                 exit(0);
                 break;
-
             case SDL_KEYDOWN:
             case SDL_KEYUP:
-                Buttons_Update(&player->buttons, event);
+                Buttons_Update(&game->world.player.buttons, event);
                 break;
-
             default:
                 break;
         }
@@ -37,10 +32,11 @@ int main(int argc, char *argv[]) {
     printf("%s Initialized.\n", GAME_TITLE);
 
     while (Game_IsRunning(&game)) {
-        Poll_Events(&game.world.player);
+        Poll_Events(&game);
         Game_Main(&game);
     }
 
+    Game_Destroy(&game);
     printf("Game Over.\n");
 
     return 0;
