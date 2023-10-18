20f55cf7d05ac6adb217ad5c209be8aba3616186
justint9696 <justin.tonkinson96@gmail.com>
2023-10-17 20:02:15 -0600

    modify hud struct & add debug text

--- src/gfx/hud.c
+++ src/gfx/hud.c
@@ -1,3 +1,4 @@
+#include "common/type.h"
 #include "game/fps.h"
 
 #include "gfx/app.h"
@@ -7,44 +8,67 @@
 
 #include <assert.h>
 #include <string.h>
-#include <SDL2/SDL_ttf.h>
 
-static int _font_height;
-static hud_s _hud;
+static Hud _hud;
 
 void Hud_Init() {
     TTF_Font *font = LoadFont(FONT_PATH, FONT_SIZE);
-
     Renderer_SetFont(font);
+    memcpy(&_hud.font, &font, sizeof(TTF_Font *));
 
-    _font_height = GetFontHeight(font);
+    _hud.height = GetFontHeight(font);
 
-    _hud.count = 0;
+    memset(&_hud.text, 0, sizeof(text_s));
+    memset(&_hud.debug, 0, sizeof(text_s));
 
     printf("Hud initialized.\n");
 }
 
-void Hud_Draw() {
-    int pos = WINDOW_HEIGHT - _font_height - 5;
-    DrawText("GALAGA", 5, pos, 0xFFFFFFFF);
+void Hud_Destroy() {
+    for (size_t i = 0; i < _hud.text.count; i++)
+        free(_hud.text.text[i]);
+
+    for (size_t i = 0; i < _hud.debug.count; i++)
+        free(_hud.debug.text[i]);
+}
+
+void Hud_Draw(bool debug) {
+    int pos = WINDOW_HEIGHT - _hud.height - 5;
+    DrawText("GALAGA", 5, pos, COLOR_WHITE);
 
     char *text;
-    for (int i = 0; i < _hud.count; i++) {
-        text = _hud.text[i];
-        pos -= _font_height + 5;
-        DrawText(text, 5, pos, 0xFFFFFFFF);
+    for (size_t i = 0; i < _hud.text.count; i++) {
+        text = _hud.text.text[i];
+        pos -= _hud.height + 5;
+        DrawText(text, 5, pos, COLOR_WHITE);
         free(text);
     }
 
-    _hud.count = 0;
+    if (debug) {
+        pos = WINDOW_HEIGHT - _hud.height - 5;
+        for (size_t i = 0; i < _hud.debug.count; i++) {
+            text = _hud.debug.text[i];
+            pos -= _hud.height + 5;
+            DrawText(text, WINDOW_WIDTH - GetFontWidth(_hud.font, text) - 5, pos, COLOR_WHITE);
+            free(text);
+        }
+    } else {
+        for (size_t i = 0; i < _hud.debug.count; i++) {
+            text = _hud.debug.text[i];
+            free(text);
+        }
+    }
+
+    _hud.text.count = 0;
+    _hud.debug.count = 0;
 }
 
 void Hud_DrawFPS(const fps_s *fps) {
-    Hud_AddText("FPS: %.2f", FPS_Get(fps));
+    Hud_AddDebugText("FPS: %.2f", FPS_Get(fps));
 }
 
 void Hud_AddText(const char *format, ...) {
-    int *count = &_hud.count;
+    int *count = &_hud.text.count;
     assert(*count < MAX_NUM_STR);
 
     va_list argptr;
@@ -56,8 +80,27 @@ void Hud_AddText(const char *format, ...) {
     size_t size = strlen(text) + 1;
     assert(size < MAX_STR_LEN);
 
-    _hud.text[*count] = (char *)malloc(sizeof(char) * size);
-    strncpy(_hud.text[*count], text, size);
+    _hud.text.text[*count] = (char *)malloc(sizeof(char) * size);
+    strncpy(_hud.text.text[*count], text, size);
+
+    ++*count;
+}
+
+void Hud_AddDebugText(const char *format, ...) {
+    int *count = &_hud.debug.count;
+    assert(*count < MAX_NUM_STR);
+
+    va_list argptr;
+    char text[MAX_STR_LEN];
+    va_start(argptr, format);
+    vsnprintf(text, MAX_STR_LEN, format, argptr);
+    va_end(argptr);
+
+    size_t size = strlen(text) + 1;
+    assert(size < MAX_STR_LEN);
+
+    _hud.debug.text[*count] = (char *)malloc(sizeof(char) * size);
+    strncpy(_hud.debug.text[*count], text, size);
 
     ++*count;
 }
--- src/gfx/hud.h
+++ src/gfx/hud.h
@@ -3,20 +3,33 @@
 
 #include "game/fps.h"
 
+#include <stdbool.h>
+
+#include <SDL2/SDL_ttf.h>
+
 #define FONT_SIZE           12
 #define FONT_PATH           "assets/fonts/press-start-2p-font/font.ttf"
 
-#define MAX_NUM_STR         8
+#define MAX_NUM_STR         12
 #define MAX_STR_LEN         32
 
 typedef struct {
     int count;
     char *text[MAX_NUM_STR];
-} hud_s;
+} text_s;
+
+typedef struct {
+    TTF_Font *font;
+    uint32_t height;
+    text_s text, debug;
+} Hud;
 
 void Hud_Init();
-void Hud_Draw();
+void Hud_Destroy();
+
+void Hud_Draw(bool debug);
 void Hud_DrawFPS(const fps_s *fps);
 void Hud_AddText(const char *format, ...);
+void Hud_AddDebugText(const char *format, ...);
 
 #endif
