1872c2dd6dc6ed4b09dbf457eb7cc23485eddefb
justint9696 <justin.tonkinson96@gmail.com>
2024-03-07 18:20:51 -0700

    proper implementation

--- src/data/array_list.c
+++ src/data/array_list.c
@@ -4,13 +4,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define get_offset(_index, _size) (_index * _size)
+#define get_offset(_index, _size) (_index * (sizeof(uint8_t) + _size))
 
 void _array_list_init(List *self, size_t item_size, size_t capacity) {
     memset(self, 0, sizeof(List));
     self->capacity = capacity;
     self->item_size = item_size;
-    self->buffer = calloc(capacity, item_size);
+    self->buffer = calloc(1, capacity * (sizeof(uint8_t) + item_size));
     assert(self->buffer);
 }
 
@@ -21,12 +21,8 @@ void array_list_destroy(List *self) {
 }
 
 void array_list_resize(List *self, size_t capacity) {
-    data_t *copy = calloc(capacity, self->item_size);
+    void *copy = realloc(self->buffer, capacity * (self->item_size + sizeof(uint8_t)));
     assert(copy);
-
-    memcpy(copy, self->buffer, self->item_size * self->capacity);
-    free(self->buffer);
-
     self->buffer = copy;
     self->capacity = capacity;
 }
@@ -38,26 +34,30 @@ void array_list_clear(List *self) {
 
 uint32_t array_list_append(List *self, void *item) {
     assert(self->size++ < self->capacity);
+
     size_t i;
-    data_t *chunk;
+    uint8_t *chunk;
     for (i = 0; i < self->capacity; i++) {
-        chunk = (self->buffer + get_offset(i, self->item_size));
-        if (!*chunk) break;
+        chunk = (uint8_t *)(self->buffer + get_offset(i, self->item_size));
+        if (!(*chunk & S_USED)) {
+            break;
+        }
     }
 
-    memcpy(chunk, item, self->item_size);
+    *chunk |= S_USED;
+    memcpy((sizeof(uint8_t) + chunk), item, self->item_size);
     return i;
 }
 
 void array_list_remove(List *self, uint32_t index) {
     assert(index < self->capacity);
-    void *chunk = (self->buffer + get_offset(index, self->item_size)); 
-    memset(chunk, 0, sizeof(self->item_size));
+    uint8_t *chunk = (uint8_t *)(self->buffer + get_offset(index, self->item_size));
+    memset(chunk, 0, sizeof(uint8_t) + self->item_size);
+    self->size--;
 }
 
 void *array_list_get(List *self, uint32_t index) {
-    assert(index < self->capacity);
-    void *chunk = (self->buffer + get_offset(index, self->item_size)); 
-    return chunk;
+    assert(index < (self->capacity));
+    uint8_t *chunk = (uint8_t *)(self->buffer + get_offset(index, self->item_size));
+    return (sizeof(uint8_t) + chunk);
 }
-
--- src/data/array_list.h
+++ src/data/array_list.h
@@ -1,24 +1,16 @@
 #ifndef _DATA_ARRAY_LIST_H_
 #define _DATA_ARRAY_LIST_H_
 
-#include "common/type.h"
-
 #include <stddef.h>
 #include <stdint.h>
 
 typedef enum {
-    S_FREE = 0,
-    S_USED = 1,
+    S_USED = 1 << 0,
 } slot_t;
 
 typedef struct {
-    slot_t slot;
-    data_t *data;
-} Chunk;
-
-typedef struct {
     size_t size, capacity, item_size;
-    Chunk *chunk;
+    uint8_t *buffer;
 } ArrayList, List;
 
 #define array_list_init(_list, _s, _size) (_array_list_init(_list, sizeof(_s), _size))
